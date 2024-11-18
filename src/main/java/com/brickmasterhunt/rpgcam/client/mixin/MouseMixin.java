5f7ca96a346fcd91d1a0013951b38e8404b35915
Brickmasterhunt <brickmasterlogan@icloud.com>
2024-11-18 16:01:20 -0500

    initial commit, need to fix the character tweaking

--- src/main/java/com/brickmasterhunt/rpgcam/client/mixin/MouseMixin.java
+++ src/main/java/com/brickmasterhunt/rpgcam/client/mixin/MouseMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Mouse.class)
 public class MouseMixin {
     @Inject(method = "lockCursor", at = @At("HEAD"), cancellable = true)
-    private void onLockCursor(CallbackInfo ci) {
+    private void lockCursor(CallbackInfo ci) {
         if (RpgCamClient.isDetachedCameraEnabled()) {
             ci.cancel();
         }
